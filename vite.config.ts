52f2574dfb0f6e1b361a81343f4bee698e0e7bdd
riceball <42161586+riceball-tw@users.noreply.github.com>
2025-11-22 22:08:39 +0800

    chore: Setup gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/auto-form-vue/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
